0a9cbbef3aaf9d9bd3732264a262d108ce812969
Sujata Saini <47734496+sujatasaini@users.noreply.github.com>
2019-07-03 22:58:00 +0530

    Create import.py

---
+++ Dropout/import.py
@@ -0,0 +1,16 @@
+#Import the library
+from __future__ import print_function
+import keras
+import datetime
+import keras.backend as K
+import numpy as np
+from keras.datasets import fashion_mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPool2D, ZeroPadding2D, BatchNormalization
+from keras import utils as np_utils
+import matplotlib.pyplot as plt
+%matplotlib inline
+np.random.seed(2019)
+
+now = datetime.datetime.now
